d32760ba7de876383f1c4f1d928c46ed980b3971
[BrianNBarton] <[Brian.n.barton@gmail.com]>
2021-02-01 16:42:23 -0700

    conditional formatting, pdf page collation

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,7 @@
 FROM ubuntu:20.04
 
 
-# RUN apt install default-jre-headless libcommons-lang3-java libbcprov-java
-# RUN apt install git default-jdk-headless ant libcommons-lang3-java libbcprov-java
-# RUN git clone https://gitlab.com/pdftk-java/pdftk.git
-# RUN cd pdftk
-# RUN mkdir libs 
-# RUN ln -st libs /usr/share/java/{commons-lang3,bcprov}.jar
-# RUN ant jar
-# RUN java -jar build/jar/pdftk.jar --help
+
 RUN apt update
 RUN echo y | apt install pdftk
 
@@ -20,10 +13,7 @@ RUN apt-get update \
     && pip3 --no-cache-dir install --upgrade pip \
     && rm -rf /var/lib/apt/lists/*
 
-# RUN apt-get install libtiff5-dev libjpeg8-dev \
-#     zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev \
-#     tk8.6-dev python-tk pdftk libmagickwand-dev
-# # We copy just the requirements.txt first to leverage Docker cache
+
 COPY ./requirements.txt /app/requirements.txt
 
 WORKDIR /app
--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from datetime import datetime
 import flask
 from pdfjinja import PdfJinja
 from flask import request, jsonify, Flask, make_response
+from PyPDF2 import PdfFileReader, PdfFileMerger
 import urllib
 import re
+import pypdftk
 import base64
 import shutil
 
@@ -13,6 +15,7 @@ app = Flask(__name__)
 
 
 def get_tooltip(name):
+
     out = name.replace(" ", "")
     out = out.replace("%", "")
     out = out.replace("$", "")
@@ -23,25 +26,71 @@ def get_tooltip(name):
     out = out.replace(".", "")
     return out
 
+# set up an elif where if it iss a string return the value or display text 
+# if it is int or decimal, grab num value, then round the decimal to the number of places specifed in places in each obj
+# for money prepend $ and for percentage suffix wit $ 
+# return all data as strings 
+
+# field_name = get_tooltip(field["name"])
+# pdf_data[field_name] = get_display_text(field)
+
+
+def get_display_text(field_object):  
+
+    display_text = field_object["numValue"]
+    # return str(field_object["numValue"])  
+    # for field in data:
+    if field_object["valueType"] == 0:
+        display_text = field_object["numValue"]
+        return display_text 
+    elif field_object["valueType"] == 1:
+        display_text = str(int(display_text))
+        # display_text = str(round(display_text,field_object["places"]))
+        return display_text
+    elif field_object["valueType"] == 2:
+        round(display_text,field_object["places"])
+        display_text = str(round(display_text,field_object["places"]))
+        return display_text
+    elif field_object["valueType"] == 3:
+        round(4,field_object["places"])
+        display_text = '$' + str(round(display_text,field_object["places"]))
+        return display_text
+    elif field_object["valueType"] == 4:
+        # print("found it")
+        round(5,field_object["places"])
+        display_text = str(round(display_text,field_object["places"])) + '%'
+        return display_text
+
 
 def generate_pdf_data(data):
+
+
+
+    # return str(x)
 
     pdf_data = {}
 
     for field in data:
-        print(get_tooltip(field["name"]))
-        if field["valueType"] == 0:
-            if not field["value"]:
-                val = "filler"
-            else:
-                val = field["value"]
+        field_name = get_tooltip(field["name"])
+        field_value = get_display_text(field)
 
-            pdf_data[get_tooltip(field["name"])] = val
-        else:
-            pdf_data[get_tooltip(field["name"])] = round(field["numValue"], 2)
+        pdf_data[field_name] = field_value
+        print(field_name,field_value)
+        # if field["valueType"] == 0:
+        #     if not field["value"]:
+        #         val = "filler"
+        #     else:
+        #         val = field["value"]
+
+        #     pdf_data[get_tooltip(field["name"])] = val
+        # else:
+        #     pdf_data[get_tooltip(field["name"])] = round(field["numValue"], 2)
 
     return pdf_data
 
+
+def debug(string):
+    print(string)
 
 @app.route("/ops", methods=["POST"])
 def generate_ops():
@@ -49,25 +98,77 @@ def generate_ops():
     print(dir(request))
     # print(request.data)
     form = json.loads(request.data)
+
+    
     data = form["OPSData"]
-    pdf_data = generate_pdf_data(data["151097_2635-1"])
+
+    # with open("./test.txt","w") as f:
+    #     for key in data:
+    #         f.write(str(key) + "\n")
+
+    output_objects = []
+    template = PdfJinja("./fillable_ops.pdf")
+
+
+    for key in data:
+        pdf_data = generate_pdf_data(data[key])
+        output_objects.append(pdf_data)
+        rendered = template(pdf_data)
+        pdf_name = "output" + key + ".pdf"
+        output_file = "./output/" + pdf_name
+
+        # append output_file to a list
+        rendered.write(open(output_file, "wb"))
+
+    # use pypdftk.concat() on the resulting list
+    output_objects = []
+    for filename in os.listdir("./output"):
+        path_name = "./output/" + filename
+        output_objects.append(path_name)
+
+    output_objects.sort(reverse=True)\
+
+
+    output = PdfFileMerger()
+
+    for pdf in output_objects:
+        pdf_file = PdfFileReader(pdf)
+        output.append(pdf_file)
+
+    # pdf_file1 = PdfFileReader("./output/outputSummary.pdf")
+    # pdf_file2 = PdfFileReader("./output/output151097_29303132-1.pdf")
+    # pdf_file3 = PdfFileReader("./output/output151097_17181920-1.pdf")
+    # output.append(pdf_file1)
+    # output.append(pdf_file2)
+    # output.append(pdf_file3)
+    output.write("outputMerged.pdf")
+
+    # generated_pdf = pypdftk.fill_form('./output/', pdf_data)
+    # output_file = pypdftk.concat(['./outputSummary/', generated_pdf])
+
+
+    # given_value = OPSData.value
+
+
+ 
 
     # form = request.form
     # data = json.loads(form["OPSData"])
     # pdf_data = generate_pdf_data(data['151097_2635-1'])
 
-    template = PdfJinja("./fillable_ops.pdf")
-    print(template)
+    # # print(template)
 
-    rendered = template(pdf_data)
-    output_file = "./output/output.pdf"
-    rendered.write(open(output_file, "wb"))
+    with open("./output.txt","w") as f:
+        json.dump(output_objects,f,indent=2)
 
-    print(form)
-    with open("./output/output.pdf", "rb") as f:
-        encoded_string = base64.b64encode(f.read())
+    
+    return flask.jsonify({"message":"hi"})
 
-    return flask.send_file("./output/output.pdf", mimetype="application/pdf", attachment_filename="ops.pdf", as_attachment=True)
+    # print(form)
+    # with open("./output/output.pdf", "rb") as f:
+    #     encoded_string = base64.b64encode(f.read())
+
+    # return flask.send_file("./output/output.pdf", mimetype="application/pdf", attachment_filename="ops.pdf", as_attachment=True)
 
     # pypdftk.fill_form("./fillable_ops.pdf",datas=data,out_file="./output/output.pdf",flatten=True)
 
@@ -82,7 +183,7 @@ def generate_ops():
     # else:
     #     print(data)
     # pdf_data = generate_pdf_data(keys[0])
-    return jsonify(data)
+    # return jsonify(data)
     # return jsonify(request.form)
 
 
